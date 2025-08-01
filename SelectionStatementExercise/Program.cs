65748fff7912d97540b1fb9e24bb927ee39cb3f7
Cecelia Coombs <chenyacc@yahoo.com>
2025-08-01 04:08:22 -0400

    complete exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace SelectionStatementExercise
 {
     public class Program
-    {
+    {     
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var favNumber = random.Next(1, 11);
+            int response = 0;
+
+            while (response != favNumber)
+            {
+                Console.WriteLine("Give me a number between 1 and 10:");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                {
+                    Console.WriteLine($"Too Low!\nYour guess: {response}");
+                }
+                else if (response > favNumber)
+                {
+                    Console.WriteLine($"Too high!\nYour guess: {response}"); 
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!\nThe favorite number was {favNumber}!");
+                }
+            }
         }
     }
 }
